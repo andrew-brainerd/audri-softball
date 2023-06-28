8b4bd5abd051eaaa6024538045c2b2e571b683d7
Andrew Brainerd <drwb333@gmail.com>
2023-06-28 18:48:54 -0400

    Working sometimes

--- win.ts
+++ win.ts
@@ -8,9 +8,11 @@ const pupOptions: PuppeteerLaunchOptions = {
 (async () => {
   const browser = await puppeteer.launch(pupOptions);
 
-  await doWinningShit(browser);
+  for (let i = 0; i < 5; i++) {
+    await doWinningShit(browser, i);
+  }
 
-  await browser.close();
+  // await browser.close();
 })();
 
 async function getPollLink(browser: Browser) {
@@ -23,7 +25,7 @@ async function getPollLink(browser: Browser) {
   console.log('Poll Link:', href);
 }
 
-async function doWinningShit(browser: Browser) {
+async function doWinningShit(browser: Browser, index: number) {
   const pollUrl = 'https://poll.fm/12449654';
   const pollPage = await openNewTab(browser, pollUrl);
 
@@ -35,9 +37,6 @@ async function doWinningShit(browser: Browser) {
       const [input] = await pollOption.$$('.css-answer-input input');
 
       if (optionText?.includes('Audri Hrncharik')) {
-        console.log('Best Athlete:', optionText);
-        
-
         await input.click();
       }
     })
@@ -46,16 +45,29 @@ async function doWinningShit(browser: Browser) {
   const [voteButtonContainer] = await pollPage.$$('.css-votebutton-outer');
   const [voteButton] = await voteButtonContainer.$$('a');
 
-  await wait(15000);
+  await wait(Math.round(Math.random() * 15));
   await voteButton.click();
 
-  await pollPage.screenshot({ path: 'result.png', fullPage: true, fromSurface: true });
+  // console.log('Vote Result:', pollPage.);
 
-  await wait(5000);
+  await pollPage
+    .screenshot({ path: `result-${index}.png`, fullPage: true, fromSurface: true, captureBeyondViewport: true })
+    .then(() => {
+      index++;
+    });
 }
 
 async function openNewTab(browser: Browser, url: string) {
   const page = await browser.newPage();
+  // await page.setRequestInterception(true);
+  await page.setExtraHTTPHeaders({
+    'user-agent':
+      'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+    'upgrade-insecure-requests': '1',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+    'accept-encoding': 'gzip, deflate, br',
+    'accept-language': 'en-US,en;q=0.9,en;q=0.8'
+  });
   await page.goto(url).catch(e => console.error('Navigation Error', e));
   await page.waitForNetworkIdle();
 
