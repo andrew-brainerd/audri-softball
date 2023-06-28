7f683b25596e52551ca0df3b2798bc87cefc5003
Andrew Brainerd <drwb333@gmail.com>
2023-06-28 18:13:38 -0400

    Click vote button

--- win.ts
+++ win.ts
@@ -1,16 +1,9 @@
-import chalk from 'chalk';
-import fs from 'fs';
-import puppeteer, { ElementHandle, Page, PuppeteerLaunchOptions } from 'puppeteer';
-import { Browser } from 'puppeteer';
+import puppeteer, { Browser, ElementHandle, PuppeteerLaunchOptions } from 'puppeteer';
 
 const pupOptions: PuppeteerLaunchOptions = {
   headless: false,
   defaultViewport: { width: 800, height: 1000 }
 };
-
-// if (process.platform === 'win32') {
-//   pupOptions.executablePath = 'C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe';
-// }
 
 (async () => {
   const browser = await puppeteer.launch(pupOptions);
@@ -33,55 +26,38 @@ async function getPollLink(browser: Browser) {
 async function doWinningShit(browser: Browser) {
   const pollUrl = 'https://poll.fm/12449654';
   const pollPage = await openNewTab(browser, pollUrl);
-  // const pollOptions = await pollPage.$x("//div[contains(@class, 'css-answer-group')]");
 
   const pollOptions = await pollPage.$$('.css-answer-row');
 
   await Promise.all(
     pollOptions.map(async pollOption => {
       const optionText = await pollOption.evaluate(fi => fi.textContent?.trim());
-      // const [input] = await pollOption.$$('input');
       const [input] = await pollOption.$$('.css-answer-input input');
 
       if (optionText?.includes('Audri Hrncharik')) {
         console.log('Best Athlete:', optionText);
-        console.log('Input', input);
         
 
         await input.click();
-        await pollOption.click();
       }
     })
   );
 
-  // const [pollLink] = await pollPage.$x("//span[contains(., 'Audri Hrncharik')]");
-  // const href = await pollLink.$eval('a', link => link.getAttribute('href'));
+  const [voteButtonContainer] = await pollPage.$$('.css-votebutton-outer');
+  const [voteButton] = await voteButtonContainer.$$('a');
 
-  // console.log('Poll Options', pollOptions);
+  await wait(15000);
+  await voteButton.click();
 
-  // if (pollLink) {
-  //   const link = pollLink as ElementHandle<Element>;
-
-  //   console.log('Clicking link', link);
-
-  //   // link.$eval("")
-
-  //   link.hover();
-  //   link.focus();
-  //   link.click(); 
-  // } else {
-  //   console.log('Clicking nothing, fuck');
-  // }
+  await pollPage.screenshot({ path: 'result.png', fullPage: true, fromSurface: true });
 
   await wait(5000);
-  // console.log("Winner Span:", pollLink);
 }
 
 async function openNewTab(browser: Browser, url: string) {
   const page = await browser.newPage();
   await page.goto(url).catch(e => console.error('Navigation Error', e));
-  await page.waitForNetworkIdle(); // { timeout: 5000 }
-  // await page.waitForNavigation({ waitUntil: 'networkidle0' });
+  await page.waitForNetworkIdle();
 
   return page;
 }
